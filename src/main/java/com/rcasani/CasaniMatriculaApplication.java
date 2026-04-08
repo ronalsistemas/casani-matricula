fa0a1262176c15b043b1f48afaba9ea470bb565a
Ronal Casani <ronalsistemas@gmail.com>
2026-04-08 17:53:19 -0500

    proyecto_base

---
+++ src/main/java/com/rcasani/CasaniMatriculaApplication.java
@@ -0,0 +1,13 @@
+package com.rcasani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaniMatriculaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasaniMatriculaApplication.class, args);
+    }
+
+}
