7890c963fbbd1333bc83ca6ea582171101a49939
Ronal Casani <ronalsistemas@gmail.com>
2026-08-02 23:16:45 -0500

    SonarQube

--- src/test/java/com/rcasani/service/EstudianteServiceTest.java
+++ src/test/java/com/rcasani/service/EstudianteServiceTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(SpringExtension.class)
-public class EstudantesServiceTest {
+public class EstudianteServiceTest {
 
     @MockitoBean
     private IEstudianteRepo estudianteRepo;
